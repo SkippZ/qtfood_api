4cb2ea1a28f0e71f5ce12caccb56936e6260a406
Marvin Knapp <marvin.knapp1@web.de>
2023-01-10 22:34:10 +0100

    created favorite storing and listing logic

---
+++ app/Http/Controllers/FavoriteController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\FavoriteCreateRequest;
+use App\Models\Favorite;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class FavoriteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       return Favorite::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(FavoriteCreateRequest $request)
+    {
+        $recipeId = $request->validated()['recipe_id'];
+        $recipeText = $request->validated()['recipe_text'];
+
+        $favorite = Favorite::firstOrCreate([
+            'recipe_id' => $recipeId,
+            'recipe_text' => $recipeText
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Favorite  $favorite
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Favorite $favorite)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Favorite  $favorite
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Favorite $favorite)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Favorite  $favorite
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Favorite $favorite)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Favorite  $favorite
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Favorite $favorite)
+    {
+        //
+    }
+}
